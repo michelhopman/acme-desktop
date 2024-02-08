b8986a9f416fe34e334518c51f8146ad712be564
Michel Hopman <michelhpmn@gmail.com>
2024-02-08 14:05:04 +0100

    final

--- app/dashboard/customers/page.jsx
+++
@@ -1,5 +0,0 @@
-function Page() {
-  return <div>Customers Page</div>;
-}
-
-export default Page;
---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+function Page() {
+  return <div>Customers Page</div>;
+}
+
+export default Page;
